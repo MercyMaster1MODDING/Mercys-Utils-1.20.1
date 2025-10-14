b2aec59e00f9d3b1af5dd361df338989b478929c
MercyMaster1MODDING <superawesome073@gmail.com>
2025-10-13 21:22:01 -0700

    Added Apple Tree Log

---
+++ src/main/java/com/mercysUtils/library/Datagen/WorldgenDatagen.java
@@ -0,0 +1,28 @@
+package com.mercysUtils.library.Datagen;
+
+import com.mercysUtils.library.MercysUtils;
+import com.mercysUtils.library.Worldgen.Biomes.ModBiomes;
+import com.mercysUtils.library.Worldgen.Biomes.ModConfiguredFeatures;
+import com.mercysUtils.library.Worldgen.Dimension.ModDimension;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+
+public class WorldgenDatagen extends DatapackBuiltinEntriesProvider {
+    public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
+            .add(Registries.DIMENSION_TYPE, ModDimension::bootstrapType)
+            .add(Registries.BIOME, ModBiomes::bootstrap)
+            .add(Registries.LEVEL_STEM, ModDimension::bootstrapStem)
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap);
+
+    public WorldgenDatagen(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
+        super(output, registries, BUILDER, Set.of(MercysUtils.MOD_ID));
+    }
+
+}
--- src/main/java/com/mercysUtils/library/Items/ModItems.java
+++ src/main/java/com/mercysUtils/library/Items/ModItems.java
@@ -24,6 +24,75 @@ public class ModItems {
     public static final RegistryObject<BlockItem> MERCINIUM_ORE_ITEM = ITEMS.register("mercinium_ore",
             () -> new BlockItem(ModBlocks.MERCINIUM_ORE.get(), new Item.Properties()));
 
+<<<<<<< Updated upstream
+=======
+    public static final RegistryObject<BlockItem> JELLYINIUM_ORE = ITEMS.register("jellyinium_ore",
+            () -> new BlockItem(ModBlocks.JELLYINIUM_ORE.get(), new Item.Properties()));
+
+    public static final RegistryObject<BlockItem> MERCINIUM_APPLE_TREE_LOG = ITEMS.register("mercinium_apple_tree_log",
+            () -> new BlockItem(ModBlocks.MERCINIUM_APPLE_TREE_LOG.get(), new Item.Properties()));
+
+    public static final RegistryObject<BlockItem> MERCINIUM_APPLE_TREE_SAPLING = ITEMS.register("mercinium_apple_tree_sapling",
+            () -> new BlockItem(ModBlocks.MERCINIUM_APPLE_TREE_SAPLING.get(), new Item.Properties()));
+
+    //Register Tools
+    public static final RegistryObject<SwordItem> MERCINIUM_SWORD = ITEMS.register("mercinium_sword",
+            () -> new SwordItem(
+                    ModTiers.MERCINIUM_TIER,
+                    10,
+                    -2.4f,
+                    new Item.Properties()));
+
+    public static final RegistryObject<PickaxeItem> MERCINIUM_PICKAXE = ITEMS.register("mercinium_pickaxe",
+            () -> new PickaxeItem(
+                    ModTiers.MERCINIUM_TIER,
+                     1,
+                    -3f,
+                    new Item.Properties()));
+
+    public static final RegistryObject<SwordItem> JELLYINIUM_SWORD = ITEMS.register("jellyinium_sword",
+            () -> new SwordItem(
+                    ModTiers.JELLYINIUM_TIER,
+                    10,
+                    -2.4f,
+                    new Item.Properties()));
+
+    public static final RegistryObject<PickaxeItem> JELLYINIUM_PICKAXE = ITEMS.register("jellyinium_pickaxe",
+            () -> new PickaxeItem(
+                    ModTiers.JELLYINIUM_TIER,
+                    1,
+                    -3f,
+                    new Item.Properties()));
+
+    //Register Armor
+    public static final RegistryObject<ArmorItem> MERCINIUM_HELMET = ITEMS.register("mercinium_helmet",
+            () -> new ArmorItem(
+                    ModArmorMaterialRegistry.MERCINIUM,
+                    ArmorItem.Type.HELMET,
+                    new Item.Properties().rarity(Rarity.EPIC)));
+
+    public static final RegistryObject<ArmorItem> MERCINIUM_CHESTPLATE = ITEMS.register("mercinium_chestplate",
+            () -> new ArmorItem(
+                    ModArmorMaterialRegistry.MERCINIUM,
+                    ArmorItem.Type.CHESTPLATE,
+                    new Item.Properties().rarity(Rarity.EPIC)));
+
+    public static final RegistryObject<ArmorItem> MERCINIUM_LEGGINGS = ITEMS.register("mercinium_leggings",
+            () -> new ArmorItem(
+                    ModArmorMaterialRegistry.MERCINIUM,
+                    ArmorItem.Type.LEGGINGS,
+                    new Item.Properties().rarity(Rarity.EPIC)));
+
+    public static final RegistryObject<ArmorItem> MERCINIUM_BOOTS = ITEMS.register("mercinium_boots",
+            () -> new ArmorItem(
+                    ModArmorMaterialRegistry.MERCINIUM,
+                    ArmorItem.Type.BOOTS,
+                    new Item.Properties().rarity(Rarity.EPIC)));
+
+
+
+
+>>>>>>> Stashed changes
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
